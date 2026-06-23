869df8e22619f69b92e90ab377d46ed208345119
Louis <louis.dubois@epitech.eu>
2026-06-23 14:45:20 +0200

    ADD: style navbar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-alert("Ceci est un casque, je suis le président de la République");
+import './styles.css'
+import logo from './images/logo.png';
+
+document.addEventListener("DOMContentLoaded", () => {
+    const logoNav = document.getElementById("logo");
+
+    if (logoNav)
+        logoNav.src = logo;
+
+    const logoLink = document.createElement("link");
+    logoLink.rel = "icon";
+    logoLink.type = "image/png";
+    logoLink.href = logo;
+    document.head.appendChild(logoLink);
+})
