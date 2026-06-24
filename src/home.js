be43d90295cf19a1d1eeb99efccdb3f787eff6a4
Louis <louis.dubois@epitech.eu>
2026-06-24 20:08:46 +0200

    ADD: home page

---
+++ src/home.js
@@ -0,0 +1,10 @@
+const home = document.createElement("div");
+
+home.className = "home-container";
+home.innerHTML = `
+    <h1>The best meat in Hyrule</h1>
+    <p>Explore the wonderful world of Hyrule in search of travel, adventure and treasure. And come and enjoy the best meat you can imagine. Lynel's rib will give you the strength to face the dangers along your path.</p>
+    <button id="home-book">Book a table</button>
+`;
+
+export { home };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
 import './styles.css'
 import logo from './images/logo.png';
 
+import { home } from './home.js';
+import { menu } from './menu.js';
+
+// Add Logo
 document.addEventListener("DOMContentLoaded", () => {
     const logoNav = document.getElementById("logo");
 
     if (logoNav)
         logoNav.src = logo;
-
-    const logoLink = document.createElement("link");
+    const logoLink = document.createElement("link")
     logoLink.rel = "icon";
     logoLink.type = "image/png";
     logoLink.href = logo;
     document.head.appendChild(logoLink);
+});
+
+const content = document.getElementById("content");
+
+const logoBtn = document.querySelector(".name");
+const homeBtn = document.getElementById("home");
+
+const menuBtn = document.getElementById("menu");
+
+const homeBookBtn = document.getElementById("home-book");
+const bookBtn = document.getElementById("book");
+
+content.innerHTML = "";
+content.appendChild(home);
+
+logoBtn.addEventListener("click", () => {
+    content.innerHTML = "";
+    content.appendChild(home);
+});
+
+homeBtn.addEventListener("click", () => {
+    content.innerHTML = "";
+    content.appendChild(home);
+});
+
+menuBtn.addEventListener("click", () => {
+    content.innerHTML = "";
+    content.appendChild(menu);
 })
